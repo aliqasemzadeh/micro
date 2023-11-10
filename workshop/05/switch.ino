a02010292caa81e0d3b617f64a8fa5f2f88dd82a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-11-10 15:02:19 +0330

    C++ all other learn C++.

---
+++ workshop/05/switch.ino
@@ -0,0 +1,18 @@
+int pushButton = 2;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+  Serial.println(buttonState);
+  delay(1);  // delay in between reads for stability
+}
